00fc3d3ed1b133c8630bdde73b85f4ccf945a822
naokita050 <49198895+naokita050@users.noreply.github.com>
2019-06-17 23:12:10 +0900

    リストの最後の要素を返す

---
+++ src/main/scala/example/P01.scala
@@ -0,0 +1,8 @@
+package example
+
+object P01 {
+  def last[T](list: List[T]): T = list match {
+    case x ::Nil => x
+    case x :: xs => last(xs)
+  }
+}
---
+++ src/test/scala/example/P01Spec.scala
@@ -0,0 +1,18 @@
+package example
+
+import org.scalatest.{DiagrammedAssertions, FunSpec}
+
+class P01Spec extends FunSpec with DiagrammedAssertions {
+  describe("P01") {
+    it("2つ以上の要素を持つリストで、最後の値を返す") {
+      assert(P01.last(List(1,2)) === 2)
+    }
+    it("1つの要素を持つリストで、唯一の要素を返す") {
+      assert(P01.last(List(1)) === 1)
+    }
+    it("複数の文字列を持つリストで、最後の要素を返す") {
+      assert(P01.last(List("1","2","3")) === "3")
+    }
+  }
+
+}
